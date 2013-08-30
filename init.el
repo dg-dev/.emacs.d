3adf386db5d592d6bc23b065c445b26846950857
Dennis G <dennis.newyork@gmail.com>
2013-08-29 22:53:22 -0400

    added monokai theme and updated init.el

--- init.el
+++ init.el
@@ -1,29 +1,36 @@
+;; hides welcome screen
 (setq inhibit-startup-message t)
 
-(set-default-font "Consolas-11")
+;; opens config in background
+;; (find-file "~/.emacs.d/init.el")
 
+;; package repositories
 (setq package-archives 
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(global-linum-mode t)             ; lines
+;; line numbers 
+(global-linum-mode t)
 
+;; quick buffer switching
 (ido-mode 1)
 (setq ido-save-directory-list-file "~/.emacs.d/cache/ido.last")
 
+;; junk 
 (setq
- backup-by-copying t              ; don't clobber symlinks
- backup-directory-alist
- '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/.emacs.d/saves"))
  delete-old-versions t
  kept-new-versions 6
  kept-old-versions 2
- version-control t)               ; use versioned backups
+ version-control t)
 
-;(setq tab-stop-list (number-sequence 4 200 4))
+;; monokai (color theme)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monokai/")
+(load-theme 'monokai t)
 
-; ace-jump-mode
+;; ace-jump-mode
 (add-to-list 'load-path "~/.emacs.d/elisp/ace-jump-mode/")
 (autoload
   'ace-jump-mode
@@ -32,13 +39,21 @@
   t)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
-; yasnippet
+;; yasnippet
 (add-to-list 'load-path "~/.emacs.d/elisp/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
 
-; executes stuff after loading
-(defun post-init ()
-  (set-frame-height (selected-frame) 40)
-  (set-frame-width (selected-frame) 140))
+;; executes stuff after initialization
+(defun post-init ())
 (add-hook 'after-init-hook 'post-init)
+
+;; windows specific stuff
+(if window-system
+    (progn 
+      ;; windows font
+      (set-default-font "Consolas-11")
+      
+      ;; frame size
+      (set-frame-size (selected-frame) 140 40)))
+
